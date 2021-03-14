07ca089251870b306c5b8e1642f5d427159a5638
carcher54321 <79350263+carcher54321@users.noreply.github.com>
2021-03-14 19:59:05 -0400

    Delete set_password.py

--- src/set_password.py
+++
@@ -1,5 +0,0 @@
-import yagmail
-import keyring
-
-yagmail.register('@gmail.com', 'your_password') # registering the email
-keyring.set_password('yagmail', 'your_email@gmail.com', 'your_password')  #setting the password
