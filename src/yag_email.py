7e74e6e07d63c1c56a62ff6773c5e8576c567b35
carcher54321 <79350263+carcher54321@users.noreply.github.com>
2021-03-14 20:05:07 -0400

    Added set_password to this file

--- src/yag_email.py
+++ src/yag_email.py
@@ -1,7 +1,14 @@
 import yagmail
+import keyring
 
 FROM_ADDRESS = 'your_email@gmail.com'
 RECIPIENTS = ['to_email@gmail.com']
+
+def set_password(email, password):
+    global FROM_ADDRESS
+    FROM_ADDRESS = email
+    yagmail.register('@gmail.com', password) # registering the email
+    keyring.set_password('yagmail', email, password)  #setting the password
 
 def sendEmail(subject, body, receiver, attachments):
     logging.info("Sending an email")
