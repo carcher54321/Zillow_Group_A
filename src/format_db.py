895e1e828912b760f7e5444e355ff7f201d798d7
firemeadow <lmi12@case.edu>
2021-04-06 10:40:30 -0400

    Updated format_db to work with staging, target tables, as well as added new cities_crosswalk data with zip codes integrated

--- src/format_db.py
+++ src/format_db.py
@@ -5,7 +5,12 @@ import logging
 
 
 def load_data(fname):
-    return pd.read_csv('../data/' + fname)
+    try:
+        d = pd.read_csv('../data/' + fname)
+    except Exception as e:
+        logging.error('Failed loading data {}. {}'.format(fname, e))
+        return False
+    return d
 
 
 def replace_header(h):
@@ -33,7 +38,7 @@ def replace_header(h):
     return h
 
 
-def get_header_type(d):
+def get_header_type(d, fname, s):
     h_init = '('
     for i, col in d.items():
         # Oracle only allows column names less than 30 characters
@@ -43,45 +48,89 @@ def get_header_type(d):
             suffix = replace_header(i.split('_')[1])
 
             header = prefix + '_' + suffix
-            if len(header) > 29:
-                print(header + str(len(header)))
 
         # Date is a reserved word
         if header == 'Date':
             header = 'Date_Column'
 
-        h_init += header + ' '
-        h_init += 'VARCHAR2(50)'
+        if not s:
+            if fname not in ['Cities_crosswalk.csv', 'CountyCrossWalk_Zillow.csv']:
+                h_init += header + '1 '
+                if header == 'Date_Column':
+                    h_init += 'DATE'
+                elif header == 'RegionName':
+                    if fname in ['City_time_series.csv', 'State_time_series.csv']:
+                        h_init += 'VARCHAR2(50)'
+                    else:
+                        h_init += 'NUMBER'
+                else:
+                    h_init += 'NUMBER'
+            else:
+                h_init += header + ' '
+                if header in ['Zip', 'StateFIPS', 'CountyFIPS', 'CountyRegionID_Zillow',
+                              'MetroRegionID_Zillow', 'FIPS', 'CBSACode']:
+                    h_init += 'NUMBER(10)'
+                else:
+                    h_init += 'VARCHAR2(50)'
+        else:
+            h_init += 'VARCHAR2(50)'
 
         if i == d.columns[-1]:
             h_init += ')'
         else:
             h_init += ', '
 
-    return h_init
-
-
-def get_d_format(n):
-    f = '('
-    for i in range(n):
-        f += ':' + str(i)
-        if i == n - 1:
-            f += ')'
+    if not s:
+        if fname == 'CountyCrossWalk_Zillow.csv':
+            h_init = h_init[0] + 'state_id NUMBER, county_id NUMBER, metro_id NUMBER, ' + h_init[1:]
+        elif fname == 'Cities_crosswalk.csv':
+            h_init = h_init[0] + 'zip_id NUMBER, city_id NUMBER, ' + h_init[1:]
         else:
-            f += ', '
-    return f
+            h_init = h_init[0] + fname.split('_')[0] + '_id NUMBER, ' + h_init[1:]
+
+    return h_init
 
 
 class Format_Db:
     def __init__(self):
         self.dsn = orc.makedsn('localhost', '1521', service_name='orcl')
-        self.user = 'zillow_group_a'
+        self.user = 'ali_adam'
         self.password = 'root'
 
-    def create_table(self, fname):
-        table_name = fname[:-4]
+    def create_table(self, fname, staging):
         data = load_data(fname)
+        if not type(data) == bool:
+            table_name = fname[:-4]
+            if staging:
+                table_name = 'S_' + table_name.upper()
+            else:
+                table_name = 'T_' + table_name.upper()
 
+            db = orc.connect(
+                user=self.user,
+                password=self.password,
+                dsn=self.dsn
+            )
+            cursor = db.cursor()
+
+            sql = "SELECT table_name FROM user_tables WHERE table_name = '" + table_name + "'"
+            cursor.execute(sql)
+
+            if cursor.fetchone():
+                logging.info('Table {} already exists.'.format(table_name))
+            else:
+                headers = get_header_type(data, fname, staging)
+                sql = 'CREATE TABLE ' + table_name + '' + headers
+                try:
+                    cursor.execute(sql)
+                    logging.info('Table {} created.'.format(table_name))
+                except Exception as e:
+                    logging.error('Failed creating table {}: {}'.format(table_name, e))
+
+            cursor.close()
+            db.close()
+
+    def create_user(self, user):
         db = orc.connect(
             user=self.user,
             password=self.password,
@@ -89,55 +138,25 @@ class Format_Db:
         )
         cursor = db.cursor()
 
-        sql = "SELECT table_name FROM user_tables WHERE table_name = '" + table_name.upper() + "'"
-        cursor.execute(sql)
-
-        if cursor.fetchone():
-            logging.info('Table {} already exists.'.format(table_name))
-        else:
-            headers = get_header_type(data)
-            sql = 'CREATE TABLE ' + table_name + '' + headers
-            try:
-                cursor.execute(sql)
-                logging.info('Table {} created.'.format(table_name))
-            except Exception as e:
-                logging.error('Failed creating table {}: {}'.format(table_name, e))
-
-        cursor.close()
-        db.close()
-        self.insert_data(data, table_name)
-
-    def insert_data(self, data, table_name):
-        db = orc.connect(
-            user=self.user,
-            password=self.password,
-            dsn=self.dsn
-        )
-        cursor = db.cursor()
-
-        sql = 'SELECT * FROM ' + table_name
-        cursor.execute(sql)
-        if cursor.fetchone():
-            logging.info('Table {} already contains data.'.format(table_name))
-        else:
-            format = get_d_format(len(data.columns))
-            try:
-                for i, row in data.iterrows():
-                    row = row.fillna('NaN')
-                    sql = 'INSERT INTO ' + table_name + ' VALUES ' + format
-                    cursor.execute(sql, tuple(row))
-                    db.commit()
-                logging.info('Data written to table {}.'.format(table_name))
-            except Exception as e:
-                logging.error('Failed inserting data into table {}: {}'.format(table_name, e))
-
-        cursor.close()
-        db.close()
+        sql1 = '''
+        CREATE USER {} IDENTIFIED BY root
+        DEFAULT TABLESPACE "ts_usr"
+        TEMPORARY TABLESPACE TEMP'''.format(user)
+        cursor.execute(sql1)
+        sql2 = 'ALTER USER {} QUOTA UNLIMITED ON "ts_usr"'.format(user)
+        cursor.execute(sql2)
+        sql3 = 'GRANT "CONNECT" TO ' + user
+        cursor.execute(sql3)
+        sql4 = 'GRANT "RESOURCE" TO ' + user
+        cursor.execute(sql4)
+        sql5 = 'GRANT CREATE VIEW TO ' + user
+        cursor.execute(sql5)
 
     def main(self):
         fnames = os.listdir('../data/')
-        for n in fnames:
-            self.create_table(n)
+        for s in [True, False]:
+            for n in fnames:
+                self.create_table(n, s)
 
 
 if __name__ == "__main__":
